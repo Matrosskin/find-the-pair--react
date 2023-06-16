da2ed955c55e91600a57222dd5748e4559b9230b
Yevhen Irkliievskyi <eugeni@default-value.com>
2023-06-16 11:45:23 +0300

    Added Stylelint. Added editorconfig.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="ftpr-app">
+      <header className="ftpr-app-header">
+        <img src={logo} className="ftpr-app-logo" alt="logo" />
         <p>
           {/* eslint-disable react/jsx-one-expression-per-line */}
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="ftpr-app-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
